f9b242065bc18e69f28ae1b41fcb347f924d5488
Huy Tran <hqtran@wpi.edu>
2017-02-08 18:46:38 -0500

    add scroll to landing page

--- src/js/custom-animate.js
+++ src/js/custom-animate.js
@@ -1,20 +1,8 @@
 $(function () {
-    var currentText = $('.count').text();
-    $('.section-panel').hover(
-        function () {
-            $('.count').each(function () {
-                var $this = $(this);
-                jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-                    duration: 1200,
-                    easing: 'swing',
-                    step: function () {
-                        $this.text(Math.ceil(this.Counter));
-                    }
-                });
-            });
-        },
-        function () {
-            $('.count').text(Math.floor(currentText / 10000));
-        }
-    );
+    $('#scrollBtn').click(function () {
+        console.log('hello');
+        $('html, body').animate({
+            scrollTop: $('#stat').offset().top
+        }, 1000);
+    });
 });
